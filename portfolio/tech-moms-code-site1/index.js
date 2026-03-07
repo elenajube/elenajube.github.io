77f90a8fc95bb7e1caf3d01ddd29ea74556e5550
elenajube <ejjube@gmail.com>
2026-03-07 12:37:55 -0700

    Delete portfolio/tech-moms-code-site1 directory

--- portfolio/tech-moms-code-site1/index.js
+++
@@ -1,4 +0,0 @@
-const greeting = document.getElementById("greeting")
-greeting.style.textAlign = "center"
-greeting.style.marginTop = "50px"
-const username = window.prompt("What's your name, Jane?")
